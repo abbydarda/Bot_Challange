ceeac77f95d8783a61f8b194f8c14a77ddf7f509
abbydarda <darda.abby@gmail.com>
2018-01-15 11:40:40 +0700

    create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,20 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+          $table->increments('id');
+          $table->string('name');
+          $table->string('username');
+          $table->string('email')->unique();
+          $table->string('street')->nullable();
+          $table->string('suite')->nullable();
+          $table->string('city');
+          $table->string('lat')->nullable();
+          $table->string('lng')->nullable();
+          $table->string('phone');
+          $table->json('company')->nullable();
+          $table->string('password');
+          $table->rememberToken();
+          $table->timestamps();
         });
     }
 
